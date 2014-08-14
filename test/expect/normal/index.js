4496e37e97a00e19b9da7b8dd8dcdea102a518b0
chencheng <sorrycc@gmail.com>
2014-08-14 16:09:57 +0800

    add testcase

---
+++ test/expect/normal/index.js
@@ -0,0 +1,7 @@
+define('normal/0.1.0/index.js', function(require, exports, module){
+var type = require("type/1.0.0/index.js");
+var relative = require('./relative');
+console.log('type: %s', type(function(){}) === 'dist');
+console.log('relative: %s', relative === 'relative');
+
+});
--- test/index.js
+++ test/index.js
@@ -1,21 +1,103 @@
+var join = require('path').join;
+var request = require('request');
+var extend = require('extend');
+var fs = require('fs');
 var SpmServer = require('../');
+
+var port = 12345;
+var server;
 
 describe('index', function() {
 
-  var server;
+  var opts;
+
+  beforeEach(function() {
+    opts = {
+      noArgvParse: true,
+      open: false,
+      port: port
+    };
+  });
 
   afterEach(function() {
     server && server.close();
   });
 
   it('normal', function(done) {
+    opts.cwd = join(__dirname, './fixtures/normal');
+    server = SpmServer(opts, function() {
+      local('seajs/seajs/2.2.0/sea.js', function(err, res, body) {
+        body.should.startWith('/*! Sea.js 2.2.0 | seajs.org/LICENSE.md */\n');
+        local('normal/0.1.0/index.js', function(err, res, body) {
+          fileEqual(body, 'normal/index.js');
+          local('jquery/1.7.2/jquery.js', function(err, res, body) {
+            body.should.startWith('define("jquery/1.7.2/jquery",[],function(e,t,n)');
+            body.should.containEql('selector:"",jquery:"1.7.2"');
+            local('type/1.0.0/index.js', function(err, res, body) {
+              fileEqual(body, 'normal/type.js');
+              local('normal/0.1.0/relative.js', function(err, res, body) {
+                fileEqual(body, 'normal/relative.js');
 
-    server = SpmServer({
-      noArgvParse: true,
-      open: false,
-      port: 12345
-    }, function() {
-      done();
+                // combo deps
+                local('??seajs/2.2.0/sea.js,jquery/1.7.2/jquery.js', function(err, res, body) {
+                  fileEqual(body, 'normal/sea_jquery.js');
+
+                  // combo all
+                  local('??seajs/2.2.0/sea.js,jquery/1.7.2/jquery.js,normal/0.1.0/index.js', function(err, res, body) {
+                    fileEqual(body, 'normal/sea_jquery_index.js');
+                    done();
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
     });
   });
+
+  it('standalone', function(done) {
+    opts.cwd = join(__dirname, './fixtures/standalone');
+    server = SpmServer(opts, function() {
+      local('a.js', function(err, res, body) {
+        body.should.be.equal('console.log(\'a: true\');\n');
+        local('normal/0.1.0/index.js', function(err, res, body) {
+          fileEqual(body, 'standalone/index.js');
+
+          // combo
+          local('??a.js,normal/0.1.0/index.js', function(err, res, body) {
+            fileEqual(body, 'standalone/a_index.js');
+            done();
+          });
+        });
+      });
+    });
+  });
+
+  it('standalone + base', function(done) {
+    opts.cwd = join(__dirname, './fixtures/standalone');
+    opts.base = 'group/project/9.9.9';
+    server = SpmServer(opts, function() {
+      local('group/project/9.9.9/index.js', function(err, res, body) {
+        fileEqual(body, 'standalone/base_index.js');
+        local('??a.js,group/project/9.9.9/index.js', function(err, res, body) {
+          fileEqual(body, 'standalone/base_a_index.js');
+          done();
+        });
+      });
+    });
+  });
+
 });
+
+function local(pathname, cb, opts) {
+  var args = {
+    url: 'http://localhost:'+port+'/'+pathname
+  };
+  request(extend(args, opts), cb);
+}
+
+function fileEqual(body, filepath) {
+  filepath = join(__dirname, 'expect', filepath);
+  body.should.be.equal(fs.readFileSync(filepath, 'utf-8'));
+}
