e147e28165c4079cd7f83378c4c16248d3aae935
chencheng <sorrycc@gmail.com>
2014-08-14 17:50:13 +0800

    update testcase

--- test/index.js
+++ test/index.js
@@ -7,90 +7,151 @@ var SpmServer = require('../');
 var port = 12345;
 var server;
 
+var defaults = {
+  noArgvParse: true,
+  open: false,
+  port: port
+};
+
 describe('index', function() {
 
-  var opts;
+  describe('normal', function() {
 
-  beforeEach(function() {
-    opts = {
-      noArgvParse: true,
-      open: false,
-      port: port
-    };
-  });
+    before(function(done) {
+      var opts = extend(defaults, {
+        cwd: join(__dirname, './fixtures/normal')
+      });
+      server = SpmServer(opts, done);
+    });
 
-  afterEach(function() {
-    server && server.close();
-  });
+    after(function() {
+      server && server.close();
+    });
 
-  it('normal', function(done) {
-    opts.cwd = join(__dirname, './fixtures/normal');
-    server = SpmServer(opts, function() {
-      local('seajs/seajs/2.2.0/sea.js', function(err, res, body) {
+    it('seajs', function(done) {
+      local('seajs/seajs/2.2.0/sea.js', function (err, res, body) {
         body.should.startWith('/*! Sea.js 2.2.0 | seajs.org/LICENSE.md */\n');
-        local('normal/0.1.0/index.js', function(err, res, body) {
-          fileEqual(body, 'normal/index.js');
-          local('jquery/1.7.2/jquery.js', function(err, res, body) {
-            body.should.startWith('define("jquery/1.7.2/jquery",[],function(e,t,n)');
-            body.should.containEql('selector:"",jquery:"1.7.2"');
-            local('type/1.0.0/index.js', function(err, res, body) {
-              fileEqual(body, 'normal/type.js');
-              local('normal/0.1.0/relative.js', function(err, res, body) {
-                fileEqual(body, 'normal/relative.js');
-
-                // combo deps
-                local('??seajs/2.2.0/sea.js,jquery/1.7.2/jquery.js', function(err, res, body) {
-                  fileEqual(body, 'normal/sea_jquery.js');
-
-                  // combo all
-                  local('??seajs/2.2.0/sea.js,jquery/1.7.2/jquery.js,normal/0.1.0/index.js', function(err, res, body) {
-                    fileEqual(body, 'normal/sea_jquery_index.js');
-
-                    // root project but 404
-                    local('normal/0.1.0/notfound.js', function(err, res, body) {
-                      res.statusCode.should.be.equal(404);
-                      done();
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
+        done();
       });
     });
+
+    it('index', function(done) {
+      local('normal/0.1.0/index.js', function(err, res, body) {
+        fileEqual(body, 'normal/index.js');
+        done();
+      });
+    });
+
+
+    it('jquery', function(done) {
+      local('jquery/1.7.2/jquery.js', function (err, res, body) {
+        body.should.startWith('define("jquery/1.7.2/jquery",[],function(e,t,n)');
+        body.should.containEql('selector:"",jquery:"1.7.2"');
+        done();
+      });
+    });
+
+    it('type', function(done) {
+      local('type/1.0.0/index.js', function(err, res, body) {
+        fileEqual(body, 'normal/type.js');
+        done();
+      });
+    });
+
+    it('relative', function(done) {
+      local('normal/0.1.0/relative.js', function(err, res, body) {
+        fileEqual(body, 'normal/relative.js');
+        done();
+      });
+    });
+
+    it('combo deps', function(done) {
+      local('??seajs/2.2.0/sea.js,jquery/1.7.2/jquery.js', function(err, res, body) {
+        fileEqual(body, 'normal/sea_jquery.js');
+        done();
+      });
+    });
+
+    it('combo all', function(done) {
+      local('??seajs/2.2.0/sea.js,jquery/1.7.2/jquery.js,normal/0.1.0/index.js', function(err, res, body) {
+        fileEqual(body, 'normal/sea_jquery_index.js');
+        done();
+      });
+    });
+
+    it('root project but 404', function(done) {
+      local('normal/0.1.0/notfound.js', function(err, res, body) {
+        res.statusCode.should.be.equal(404);
+        done();
+      });
+    });
+
   });
 
-  it('standalone', function(done) {
-    opts.cwd = join(__dirname, './fixtures/standalone');
-    server = SpmServer(opts, function() {
+  describe('standalone', function() {
+
+    before(function(done) {
+      var opts = extend(defaults, {
+        cwd: join(__dirname, './fixtures/standalone')
+      });
+      server = SpmServer(opts, done);
+    });
+
+    after(function() {
+      server && server.close();
+    });
+
+    it('a.js', function(done) {
       local('a.js', function(err, res, body) {
         body.should.be.equal('console.log(\'a: true\');\n');
-        local('normal/0.1.0/index.js', function(err, res, body) {
-          fileEqual(body, 'standalone/index.js');
-
-          // combo
-          local('??a.js,normal/0.1.0/index.js', function(err, res, body) {
-            fileEqual(body, 'standalone/a_index.js');
-            done();
-          });
-        });
+        done();
       });
     });
+
+    it('index', function(done) {
+      local('normal/0.1.0/index.js', function(err, res, body) {
+        fileEqual(body, 'standalone/index.js');
+        done();
+      });
+    });
+
+    it('combo', function(done) {
+      local('??a.js,normal/0.1.0/index.js', function(err, res, body) {
+        fileEqual(body, 'standalone/a_index.js');
+        done();
+      });
+    });
+
   });
 
-  it('standalone + base', function(done) {
-    opts.cwd = join(__dirname, './fixtures/standalone');
-    opts.base = 'group/project/9.9.9';
-    server = SpmServer(opts, function() {
+  describe('standalone + base', function() {
+
+    before(function(done) {
+      var opts = extend(defaults, {
+        cwd: join(__dirname, './fixtures/standalone'),
+        base: 'group/project/9.9.9'
+      });
+      server = SpmServer(opts, done);
+    });
+
+    after(function() {
+      server && server.close();
+    });
+
+    it('index', function(done) {
       local('group/project/9.9.9/index.js', function(err, res, body) {
         fileEqual(body, 'standalone/base_index.js');
-        local('??a.js,group/project/9.9.9/index.js', function(err, res, body) {
-          fileEqual(body, 'standalone/base_a_index.js');
-          done();
-        });
+        done();
       });
     });
+
+    it('combo', function(done) {
+      local('??a.js,group/project/9.9.9/index.js', function(err, res, body) {
+        fileEqual(body, 'standalone/base_a_index.js');
+        done();
+      });
+    });
+
   });
 
 });
